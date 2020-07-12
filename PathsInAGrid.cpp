4298643f5b016000bfb8b397cb9b461c03465135
MarkCaii <caimark93@gmail.com>
2020-07-11 20:51:35 -0400

    Fixed DP algorithm

--- PathsInAGrid.cpp
+++ PathsInAGrid.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-	int n, m, x, y;
+	int n, m, x = 0, y = 0;
 	cin >> n >> m;
 	string line;
 	int dp[n][m];
@@ -14,15 +14,22 @@ int main(){
 		for(int j = 0; j < m; j++){
 			if(line[m] == '*'){
 				dp[i][j] = -1;
-			}else{
+			}else if(dp[i][j] != 1){
 				dp[i][j] = 0;
 			}
 		}
 	}
+	
 	for(int i = n-2; i >= 0; i--){
 		for(int j = 1; j < m; j++){
-			x = dp[i][j-1];
-			y = dp[i+1][j];
+			if(j-1 == 0)
+				x = 1;
+			else
+				x = dp[i][j-1];
+			if(i+1 == n-1)
+				y = 1;
+			else
+				y = dp[i+1][j];
 			if(x != -1)
 				dp[i][j] += x;
 			if(y != -1)
