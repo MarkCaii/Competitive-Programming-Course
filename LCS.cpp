6ac1ca7537b34db2cfcee475e6965bbdda4d6546
MarkCaii <caimark93@gmail.com>
2020-07-13 01:21:41 -0400

    Finished LCS

--- LCS.cpp
+++ LCS.cpp
@@ -1,22 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 int m, n;
-dp[m][n]
+int dp[10000][10000];
 int LCSW(int x, int y, string s, string t){
 	if(x == m){
-		if()
+		if(s[x] == t[y]){
+			return 1;
+		}else{
+			return 0;
+		}
+	}
+	if(y == n){
+		if(t[y] == s[x]){
+			return 1;
+		}else{
+			return 0;
+		}
 	}
 	if(s[x] == t[y]){
-		dp[x][y] = 1 + LCSW(x+1, y+1, s, t);
-	}else if(){
-
+		if(dp[x][y] == 0){
+			dp[x][y] = 1 + LCSW(x+1, y+1, s, t);
+		}
+		return dp[x][y];
+	}else{
+		return 0;
 	}
 }
 int main(){
-	int m, n, i, j;
+	int i, j, max;
 	string s, t;
 	cin >> m >> n;
-	cin >> s cin >> t;	
-	cout << LCSW(m, n, s, t) << endl;	
+	cin >> s >> t;
+	for(i = 0; i < n; i++){
+		for(j = 0; j < m; j++){
+			int temp = LCSW(j, i, s, t);
+			if(temp > max){
+				max = temp;	
+			}
+		}
+	}
+	cout << max << endl;	
 	return 0;
 }
